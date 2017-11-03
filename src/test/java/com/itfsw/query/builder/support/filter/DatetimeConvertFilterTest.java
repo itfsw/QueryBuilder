527bc154aff91f01602bf53ff2e8b66557f29669
hewei <hewei@itfsw.com>
2017-11-03 15:31:23 +0800

    测试用例:DatetimeConvertFilter

--- src/test/java/com/itfsw/query/builder/support/filter/DatetimeConvertFilterTest.java
+++ src/test/java/com/itfsw/query/builder/support/filter/DatetimeConvertFilterTest.java
@@ -16,18 +16,23 @@
 
 package com.itfsw.query.builder.support.filter;
 
+import com.itfsw.query.builder.MongodbQueryBuilderFactory;
 import com.itfsw.query.builder.SqlQueryBuilderFactory;
 import com.itfsw.query.builder.exception.FilterException;
 import com.itfsw.query.builder.other.FileHelper;
+import com.itfsw.query.builder.support.builder.MongodbBuilder;
 import com.itfsw.query.builder.support.builder.SqlBuilder;
+import com.itfsw.query.builder.support.model.result.MongodbQueryResult;
 import com.itfsw.query.builder.support.model.result.SqlQueryResult;
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.sql.Date;
-import java.sql.Time;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * ---------------------------------------------------------------------------
@@ -51,13 +56,13 @@ public class DatetimeConvertFilterTest {
 
         Assert.assertEquals("datetime = ? AND date = ? AND time = ?", result.getQuery());
 
-        Assert.assertTrue(result.getParams().get(0) instanceof Date);
+        Assert.assertTrue(result.getParams().get(0) instanceof java.sql.Date);
         Assert.assertEquals("2017-11-03 14:25:12", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(0)));
 
-        Assert.assertTrue(result.getParams().get(1) instanceof Date);
+        Assert.assertTrue(result.getParams().get(1) instanceof java.sql.Date);
         Assert.assertEquals("2017-11-15 00:00:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(1)));
 
-        Assert.assertTrue(result.getParams().get(2) instanceof Time);
+        Assert.assertTrue(result.getParams().get(2) instanceof java.sql.Time);
         Assert.assertEquals("1970-01-01 14:30:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(2)));
 
         Assert.assertEquals("datetime = '2017-11-03 14:25:12' AND date = '2017-11-15 00:00:00' AND time = '14:30:00'", result.getQuery(true));
@@ -72,5 +77,53 @@ public class DatetimeConvertFilterTest {
         SqlBuilder builder = factory.builder();
         String json = FileHelper.getStringFrom("tasks/type-datetime-error.json");
         builder.build(json);
+    }
+
+    /**
+     * 测试参数有list的情况
+     */
+    @Test
+    public void testTypeList() throws IOException {
+        MongodbQueryBuilderFactory factory = new MongodbQueryBuilderFactory();
+        MongodbBuilder builder = factory.builder();
+        String json = FileHelper.getStringFrom("tasks/type-datetime-list.json");
+        MongodbQueryResult result = builder.build(json);
+
+        BasicDBObject item = (BasicDBObject) ((DBObject) (((BasicDBList) (result.getQuery().get("$and"))).get(1))).get("date");
+        Assert.assertTrue(item.get("$gte") instanceof Date);
+        Assert.assertTrue(item.get("$lte") instanceof Date);
+        Assert.assertEquals("2017-11-01 00:00:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(item.get("$gte")));
+        Assert.assertEquals("2017-11-23 00:00:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(item.get("$lte")));
+    }
+
+    /**
+     * 测试自定义
+     */
+    @Test
+    public void testCustom() throws IOException {
+        SqlQueryBuilderFactory factory = new SqlQueryBuilderFactory();
+
+        factory.addFilterAt(new DatetimeConvertFilter(
+                new SimpleDateFormat("yyyy/MM/dd HH-mm-ss"),
+                new SimpleDateFormat("yyyy/MM/dd"),
+                new SimpleDateFormat("HH-mm-ss")
+        ), DatetimeConvertFilter.class);
+
+        SqlBuilder builder = factory.builder();
+        String json = FileHelper.getStringFrom("tasks/type-datetime-custom.json");
+        SqlQueryResult result = builder.build(json);
+
+        Assert.assertEquals("datetime = ? AND date = ? AND time = ?", result.getQuery());
+
+        Assert.assertTrue(result.getParams().get(0) instanceof java.sql.Date);
+        Assert.assertEquals("2017-11-03 14:25:12", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(0)));
+
+        Assert.assertTrue(result.getParams().get(1) instanceof java.sql.Date);
+        Assert.assertEquals("2017-11-15 00:00:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(1)));
+
+        Assert.assertTrue(result.getParams().get(2) instanceof java.sql.Time);
+        Assert.assertEquals("1970-01-01 14:30:00", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(result.getParams().get(2)));
+
+        Assert.assertEquals("datetime = '2017-11-03 14:25:12' AND date = '2017-11-15 00:00:00' AND time = '14:30:00'", result.getQuery(true));
     }
 }
